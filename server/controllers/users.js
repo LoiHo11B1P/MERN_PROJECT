b0241fe91f3c1e0186a48e139bd4460a4231e1dd
Vinh Pham <phamv1@unlv.nevada.edu>
2022-03-28 20:15:51 -0700

    api to create an retrieve user water and calories

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,25 +3,7 @@ const user = express.Router()
 
 const db = require('../models')
 
-// GET ALL NON-PRIVATE USERS
-user.get('/', async(req, res) => {
-    try {
-        const allUserData = await db.User.find({
-            $or: [
-                {isPrivate: false},
-                {isPrivate: undefined}
-            ]
-        }).select('-_id -pin -email -isPrivate')
-        res.status(200).json({
-            success: true,
-            message: 'Retrieved all non-private user information.', 
-            data: allUserData
-        })
-    } catch (error) {
-        res.status(500).json()
-    }
-    
-})
+
 
 // LOGIN USING EMAIL AND PIN
 user.post('/login', async(req, res) => {
@@ -104,4 +86,127 @@ user.delete('/:id/remove', async(req, res) => {
     }
 })
 
+// Input water
+user.post('/water/:id', async(req, res) => {
+    try {
+        const currentUser = await db.User.findById(req.params.id)
+
+        const currentWaterIntake = await db.Water.create(req.body)
+
+        currentUser.waterIntake.push(currentWaterIntake.id)
+        currentUser.save()
+
+        res.status(200).json({
+            success: true,
+            message: `Successfully record water intake for user ${currentUser.name}`,
+            data: currentWaterIntake
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to add water intake."
+        })
+    }
+})
+
+// Get A List of Water
+user.get('/water/:id&:timePeriod', async (req, res) => {
+    
+    try {
+        db.User.findById(req.params.id)
+        .populate({
+            path: "waterIntake",
+            match: { dateTime: {$gte: req.params.timePeriod} }
+        })
+        .then(water => {
+            res.status(200).json({
+                success: true,
+                message: `Retrieve water recorded.`,
+                data: water.waterIntake
+            })
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to get water recorded.",
+            data: error
+        })
+    }
+    
+
+})
+
+// Input Calories
+user.post('/calories/:id', async(req, res) => {
+    try {
+        const currentUser = await db.User.findById(req.params.id)
+
+        const currentCaloriesIntake = await db.Calories.create(req.body)
+
+        currentUser.caloriesIntake.push(currentCaloriesIntake.id)
+        currentUser.save()
+
+        res.status(200).json({
+            success: true,
+            message: `Successfully record calories intake for user ${currentUser.name}`,
+            data: currentCaloriesIntake
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to add calories intake."
+        })
+    }
+})
+
+// Get A List of Calories
+user.get('/calories/:id&:timePeriod', async (req, res) => {
+    
+    try {
+        db.User.findById(req.params.id)
+        .populate({
+            path: "caloriesIntake",
+            match: { dateTime: {$gte: req.params.timePeriod} }
+        })
+        .then(calories => {
+            res.status(200).json({
+                success: true,
+                message: `Retrieve calories recorded.`,
+                data: calories.caloriesIntake
+            })
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to get calories recorded.",
+            data: error
+        })
+    }
+    
+
+})
+
+// GET ALL NON-PRIVATE USERS
+user.get('/', async(req, res) => {
+    try {
+        const allUserData = await db.User.find({
+            $or: [
+                {isPrivate: false},
+                {isPrivate: undefined}
+            ]
+        }).select('-_id -pin -email -isPrivate')
+        res.status(200).json({
+            success: true,
+            message: 'Retrieved all non-private user information.', 
+            data: allUserData
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to get users records.",
+            data: error
+        })
+    }
+    
+})
 module.exports = user
