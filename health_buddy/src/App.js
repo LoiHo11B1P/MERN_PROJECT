442ec3df97c31cb5bff102c1c6d773c2d122dd19
Vinh Pham <phamv1@unlv.nevada.edu>
2022-04-02 00:26:14 -0700

    font-end

--- health_buddy/src/App.js
+++ health_buddy/src/App.js
@@ -4,11 +4,12 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Update from './pages/Update'
-
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
     <div>
+      <UserProvider>
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login/>} />
@@ -17,7 +18,7 @@ function App() {
                 <Route path="/update" element={<Update/>} /> 
             </Routes>
         </BrowserRouter>
-
+      </UserProvider>
         
     </div>
   )
--- health_buddy/src/pages/Login.js
+++ health_buddy/src/pages/Login.js
@@ -1,30 +1,79 @@
-import {useState} from 'react'
+import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Login.css'
+import { UserContext } from "../contexts/UserContext";
 
 function App() {
-    const [name, setName] =useState('')
+
+    const navigate = useNavigate();
+    
     const [email, setEmail] =useState('')
     const [password, setPassword] =useState('')
-  
+    
+    const { setUser } = useContext(UserContext)
+
+    const [ loginMessage, setLoginMessage ] = useState()
+
+    // login
+    const onLogin = e => {
+        e.preventDefault()
+
+        attemptLogin(email, password)
+        
+    }
+
+    // contact api to login
+    const attemptLogin = async(email, pin) => {
+        const url = 'https://frozen-plateau-93848.herokuapp.com/users/login'
+        let response = await fetch(url,
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email: email, pin: pin})
+            }    
+        )
+
+        const result = await response.json();
+        if(result && result.data.length > 0) {
+
+            setUser(result.data[0])
+
+            navigate("/")
+
+
+        } else {
+            console.log("Invalid User Name or Password")
+            setLoginMessage ("Invalid User Name or Password")
+        }
+        
+    }
   
     return (
         <div className="body">
             <div className="logo"></div>
             <div className="login-container" >
                 <h2 className="Heading2">Login</h2>
-                <form className="form">
+                <p id="loginMessage">
+                    { loginMessage }
+                </p>
+                <form className="form" onSubmit={ onLogin }>
                     <input
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        type="text"
+                        type="email"
                         placeholder="Email"
+                        required
                     />
                     <br />
                     <input
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        type="text"
+                        type="password"
                         placeholder="Password"
+                        required
                     />
                     <br /> 
                     <h4> Don't have an account yet? 
