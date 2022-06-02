d181f35750b845945ee17856dbe80f8d7a8955ef
Vinh Pham <phamv1@unlv.nevada.edu>
2022-06-01 18:23:49 -0700

    update server to look for client build

--- server/server.tsx
+++ server/server.tsx
@@ -13,6 +13,12 @@ const PORT = process.env.PORT || 3001
 app.use(cors());
 app.use(express.json()); // to parse request body json
 
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
+
+
 // Mongoose
 mongoose.connect(process.env.MONGO_URI!, {}, 
     () => { console.log('connected to mongo on 3001 ') }
